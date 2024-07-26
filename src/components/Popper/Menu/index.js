4bd8152b94da91c511b293790a848486f8cbac60
tVhowww <nguyentrunghau7746@gmai.com>
2024-07-26 22:01:39 +0700

    fix bug #2

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -57,7 +57,7 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn 
                                 }}
                             />
                         )}
-                        {renderItems()}
+                        <div className={cx('menu-body')}>{renderItems()}</div>
                     </PopperWrapper>
                 </div>
             )}
